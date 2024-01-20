22a1bbcc1a2eb0860a545bf85a1e9ff6d0728333
sharif-github <107545661+sharif-github@users.noreply.github.com>
2024-01-20 13:47:14 +0600

    navbar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import logo from '../assets/logo.png';
 import moment from 'moment';
 import Button from 'react-bootstrap/Button';
 import Marquee from 'react-fast-marquee';
+import { Link } from 'react-router-dom';
+import userIcon from '../assets/user.png'
 
 const Header = () => {
     return (
@@ -13,11 +15,23 @@ const Header = () => {
                 <p className='text-secondary fw-semibold'>{moment().format('dddd, MMMM D, YYYY')}</p>
             </div>
             <div className='bg-secondary-subtle p-3 w-75 mx-auto d-flex'>
-                <Button variant="danger">Danger</Button>
-                <Marquee className='ms-3' pauseOnHover speed={'100'}>
+                <Button variant="danger" className='rounded-0'>Latest</Button>
+                <Marquee className='ms-3' pauseOnHover speed={'50'}>
                     Match Highlights: Germany vs Spain — as it happened!   Match Highlights: Germany vs Spain.
                 </Marquee>
             </div>
+            <nav className='mt-5 d-flex align-items-center row w-75 mx-auto'>
+                <div className='col'></div>
+                <div className='mx-auto col text-center'>
+                    <Link to={'/'} className='text-decoration-none me-3 text-secondary'>Home</Link>
+                    <Link to={'/'} className='text-decoration-none me-3 text-secondary'>About</Link>
+                    <Link to={'/'} className='text-decoration-none me-3 text-secondary'>Career</Link>
+                </div>
+                <div className='text-end col pe-0'>
+                    <img src={userIcon} style={{width: '37px'}} alt="" />
+                    <Button className="btn btn-secondary ms-2 me-0 rounded-0">Login</Button>
+                </div>
+            </nav>
         </div>
     );
 };
